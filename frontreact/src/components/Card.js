06aea07981d4eae201144efe29a5627008ba9584
Jhonatan Alarcón <jhonatan.alarcon.ariza@gmail.com>
2021-12-03 10:53:10 -0500

    Initial commit :tada:

--- frontreact/src/components/Card.js
+++ frontreact/src/components/Card.js
@@ -8,10 +8,7 @@ import React from 'react';
 export const Card = (props) => {
   const { name, picture, price, isActive } = props;
   return (
-    <div
-      className={isActive ? 'card' : 'card inactive'}
-      tabIndex={isActive ? '0' : '-1'}
-    >
+    <div>
       <div className='card-header'>
         <p className='card-title'>{name}</p>
         <span className='material-icons card-favorite'>favorite_border</span>
